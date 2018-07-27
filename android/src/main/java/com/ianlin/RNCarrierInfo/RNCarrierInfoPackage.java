a793d5e2263d69e54e40f9a4d305146c10ae6d3f
ZJ <ReachZJLin@gmail.com>
2018-07-26 17:25:14 -0700

    Update RNCarrierInfoPackage.java
    RNCarrierInfoPackage - removed deprecated createJSModules

--- android/src/main/java/com/ianlin/RNCarrierInfo/RNCarrierInfoPackage.java
+++ android/src/main/java/com/ianlin/RNCarrierInfo/RNCarrierInfoPackage.java
@@ -19,11 +19,6 @@ public class RNCarrierInfoPackage implements ReactPackage {
         return modules;
     }
 
-    // Deprecate RN 0.47
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
